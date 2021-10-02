af9416907bee19790e2c2041de0443fccd538a1e
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-02 12:14:05 +0200

    #3 - Create a controller using inheritance

---
+++ Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Biglidio\BestSelling\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        die('Biglidio - Best Selling - Index');
+    }
+}
