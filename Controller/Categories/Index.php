8e683b3e743571eb1d6000e74ccf3a2ebe0936c1
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-03 12:33:11 +0200

    #6 - Access the request object

--- Controller/Categories/Index.php
+++ Controller/Categories/Index.php
@@ -4,13 +4,18 @@ namespace Biglidio\BestSelling\Controller\Categories;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\HTTP\PhpEnvironment\Request;
 
 class Index extends Action
 {
     public function execute()
     {
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Categories');
+        /** @var Request $request */
+        $request = $this->getRequest();
+        $categoryId = $request->getParam('category_id');
+        $limit = $request->getParam('limit');
+        $result->setContents("category_id: $categoryId, limit: $limit");
         return $result;
     }
 }
--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,13 +4,16 @@ namespace Biglidio\BestSelling\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\HTTP\PhpEnvironment\Request;
 
 class Index extends Action
 {
     public function execute()
     {
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Index');
+        /** @var Request $request */
+        $request = $this->getRequest();
+        $result->setContents($request->getControllerName());
         return $result;
     }
 }
