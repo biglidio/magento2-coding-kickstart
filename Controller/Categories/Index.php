28a494b1ab1b7d46df4f4a083a732bd010b732d7
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-04 19:43:07 +0200

    #28 - Instantiate a collection with a factory

--- Controller/Categories/Index.php
+++ Controller/Categories/Index.php
@@ -3,11 +3,24 @@
 namespace Biglidio\BestSelling\Controller\Categories;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\HTTP\PhpEnvironment\Request;
+use Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection as BestsellersCollection;
+use Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory as BestsellersCollectionFactory;
 
 class Index extends Action
 {
+    protected $bestsellersCollectionFactory;
+
+    public function __construct(
+        Context $context,
+        BestsellersCollectionFactory $bestsellersCollectionFactory
+    ) {
+        $this->bestsellersCollectionFactory = $bestsellersCollectionFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
@@ -15,6 +28,10 @@ class Index extends Action
         $request = $this->getRequest();
         $categoryId = $request->getParam('category_id');
         $limit = $request->getParam('limit');
+
+        /** @var BestsellersCollection $bestsellersCollectionFactory */
+        $bestsellersCollectionFactory = $this->bestsellersCollectionFactory->create();
+
         $result->setContents("category_id: $categoryId, limit: $limit");
         return $result;
     }
